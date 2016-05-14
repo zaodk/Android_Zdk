0f3c785aaaf01ad12e62deff609fef6b5597ba83
javalong <javalong@xunbaozl.com>
2016-05-14 11:37:03 +0800

    模块名称修改 day08提交

---
+++ day_08_LinearLayout线性布局(一)/src/main/java/com/javalong/zdk/day_08/MainActivity.java
@@ -0,0 +1,12 @@
+package com.javalong.zdk.day_08;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vertical_linearlayout);
+    }
+}
