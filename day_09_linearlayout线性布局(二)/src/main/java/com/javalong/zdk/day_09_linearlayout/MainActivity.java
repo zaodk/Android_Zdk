7f6d81d7577c434415f11480d463fadc5e52c952
javalong <javalong@xunbaozl.com>
2016-05-20 23:09:40 +0800

    第9天 LinearLayout线性布局(二)

---
+++ day_09_linearlayout线性布局(二)/src/main/java/com/javalong/zdk/day_09_linearlayout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.javalong.zdk.day_09_linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
